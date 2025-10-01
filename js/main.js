f26118a8c1ea8afe10bda1be743868ab50fa00d7
Louise Lawrence <loumlawrence@gmail.com>
2025-10-02 01:41:05 +0930

    Code opens app within the same page

--- js/main.js
+++ js/main.js
@@ -4,9 +4,44 @@ function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 }
 
+function hideElementsByClass(classname) {
+    elements = document.getElementsByClassName(classname);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+    }
+}
 
+function showElementsByClass(classname) {
+    elements = document.getElementsByClassName(classname);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "block";
+    }
+}
+
+// https://stackoverflow.com/questions/66405508/how-do-you-make-a-canvas-change-size-when-resizing-and-remove-side-bar
+// https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
+
+function createCanvas() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    canvas = document.createElement("canvas");
+
+
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.left = "0px";
+    canvas.style.top = "0px";
+    canvas.style.absolute = "0px";
+
+    canvas.id = "canvas";
+    document.body.appendChild(canvas);
+    console.log(document.getElementById("canvas"));
+}
 
 function openWebApp() {
-    filename = "app.html"
-    window.open("file:app.html")
+    // hide elements
+    hideElementsByClass("intro");
+    // create a canvas
+    createCanvas();
 }
