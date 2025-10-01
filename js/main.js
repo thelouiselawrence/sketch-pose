c9627bdcb56572535008ac51faf6cb21a8b06da6
Louise Lawrence <loumlawrence@gmail.com>
2025-10-01 23:59:44 +0930

    Dark mode toggle

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// code for basic button responses on the main page
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    //toggleDarkModeButton = document.getElementById("dark-mode_toggle");
+    //toggleDarkModeButton.addEventListener("click", function() {
+    //});
+}
